c67d7ee3f47735ebef7d04d348f0afdd86a08e31
Luis Zuniga <lufezuni@espol.edu.ec>
2017-07-12 18:54:46 -0500

    formulario POST

--- Formularios/TallerForms/forms/urls.py
+++ Formularios/TallerForms/forms/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^editar/$', views.editarPerfil, name='editarPerfil'),
+    url(r'^agregar/$', views.agregarPlatillo, name='agregarPlatillo'),
+    url(r'^miPerfil/$', views.miPerfil, name='miPerfil'),
+    url(r'^platillos/$', views.platilloAgregado, name='platilloAgregado'),
 ]
--- Formularios/TallerForms/forms/views.py
+++ Formularios/TallerForms/forms/views.py
@@ -10,3 +10,54 @@ def index(request):
         'text': test,
     }
     return HttpResponse(template.render(context, request))
+
+
+def agregarPlatillo(request):
+    template = loader.get_template('forms/AgregarPlatillo.html')
+    test = 'prueba'
+    context = {
+        'text': test,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def editarPerfil(request):
+    return HttpResponse('holis2')
+
+
+
+def platilloAgregado(request):
+    template = loader.get_template('forms/platillos.html')
+    test = 'prueba'
+    if request.method == 'POST':
+        nombre = request.POST.get("nombreP","")
+        desc = request.POST.get("descripcion", "")
+        nutricion = request.POST.get("nutri", "")
+        tipo = request.POST.get("tipo", "")
+        cuadro = ''
+        if(nutricion == 'on'):
+            cuadro = 'cuadro deshabilitado'
+        else:
+            cuadro = 'cuadro Nutricional deshabilitado'
+        tipoA = ''
+        if(tipo == '1'):
+            tipoA = 'Almuerzo'
+        elif(tipo == '2'):
+            tipoA = 'Desayuno'
+        elif(tipo == '3'):
+            tipoA = 'Plato a la carta'
+
+    else:
+        nombre = 'default value'
+    context = {
+        'nombreP': nombre,
+        'desc': desc,
+        'nutricion': cuadro,
+        'tipo': tipoA,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def miPerfil(request):
+    return HttpResponse('holis4')
